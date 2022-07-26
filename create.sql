5a3f2a74a8325823c886b1600528967fb84f082f
dkatina <108095842+dkatina@users.noreply.github.com>
2022-07-26 18:10:07 -0400

    done

---
+++ create.sql
@@ -0,0 +1,49 @@
+
+
+-- DROP TABLE IF EXISTS customer CASCADE;
+-- CREATE TABLE customer(
+--     id SERIAL PRIMARY KEY,
+--     customer_name VARCHAR(50) NOT NULL,
+--     email VARCHAR(50) NOT NULL
+-- );
+
+-- DROP TABLE IF EXISTS theatre CASCADE;
+-- CREATE TABLE theatre(
+--     id SERIAL PRIMARY KEY,
+--     theatre_name VARCHAR(50) NOT NULL,
+--     loc VARCHAR(50) NOT NULL
+-- );
+
+-- DROP TABLE IF EXISTS movie CASCADE;
+-- CREATE TABLE movie(
+--     id SERIAL PRIMARY KEY,
+--     theater_id INTEGER,
+--     movie_title VARCHAR(20) NOT NULL,
+--     descrip TEXT NOT NULL,
+--     FOREIGN KEY (theater_id) REFERENCES theatre(id) ON DELETE CASCADE
+-- );
+
+-- DROP TABLE IF EXISTS concession CASCADE;
+-- CREATE TABLE concession(
+--     id SERIAL PRIMARY KEY,
+--     concession_name VARCHAR(50) NOT NULL,
+--     price DECIMAL(6,2)
+-- );
+
+-- DROP TABLE IF EXISTS purchase CASCADE;
+-- CREATE TABLE purchase(
+--     id SERIAL PRIMARY KEY,
+--     concession_id INTEGER,
+--     customer_id INTEGER,
+--     FOREIGN KEY (concession_id) REFERENCES concession(id) ON DELETE CASCADE,
+--     FOREIGN KEY (customer_id) REFERENCES customer(id) ON DELETE CASCADE
+-- );
+
+-- DROP TABLE IF EXISTS ticket CASCADE;
+-- CREATE TABLE ticket(
+--     id SERIAL PRIMARY KEY,
+--     movie_id INTEGER,
+--     customer_id INTEGER,
+--     FOREIGN KEY (movie_id) REFERENCES movie(id) ON DELETE CASCADE,
+--     FOREIGN KEY (customer_id) REFERENCES customer(id) ON DELETE CASCADE
+-- );
